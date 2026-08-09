40a65967f08170ce1be2fff7eeb80fb9bb83fda9
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-10 01:44:27 +0700

    Cú pháp JSX trong ReactJS - Phần 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,66 @@
 import "./App.css";
 
-const male = "male";
+/**
+ * 1. Render có điều kiện
+ *  - dùng if else
+ *  - dùng toán tử 3 ngôi
+ * 2. Render với vòng lặp (map)
+ *
+ */
 
-const myAlert = () => {
-  alert("You clicked the link!");
-};
+function getProduct(product) {
+  if (product.status === 1) {
+    return (
+      <>
+        <h3>
+          Name: {product.name} - ID: {product.id}
+        </h3>
+        <p>Price: ${product.price}</p>
+        <p>Description: {product.description}</p>
+        <p>
+          Status:{" "}
+          <span style={{ color: product.status === 1 ? "green" : "red" }}>
+            {product.status === 1 ? "Còn hàng" : "Hết hàng"}
+          </span>
+        </p>
+      </>
+    );
+  }
+}
 
-const welcomeMessage = "Welcome to my React application!";
+const products = [
+  {
+    id: 1,
+    name: "Iphone 14",
+    price: 2000,
+    description: "Sản phẩm mới nhất của Apple",
+    status: 1,
+  },
+  {
+    id: 2,
+    name: "Iphone 13",
+    price: 1500,
+    description: "Sản phẩm cũ của Apple",
+    status: 0,
+  },
+  {
+    id: 3,
+    name: "Iphone 12",
+    price: 1000,
+    description: "Sản phẩm cũ của Apple",
+    status: 1,
+  },
+];
+
+const productList = products.map((product) => {
+  return <div key={product.id}>{getProduct(product)}</div>;
+});
 
 function App() {
   return (
     <>
-      <div className="block">
-        <h2 className="sub-title" id="sub-title">
-          {welcomeMessage}
-        </h2>
-        <h1 className="title" id="title">
-          React + Vite
-        </h1>
-        <p className="description" id="description">
-          This is a simple React application created using Vite.{" "}
-          <a
-            onClick={myAlert}
-            style={{ color: "green" }}
-            href="https://vitejs.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn more about Vite
-          </a>
-          .
-        </p>
-      </div>
-
-      <label htmlFor="name">Name:</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        defaultValue="cuongvu@gmail.com"
-        placeholder="Enter your name"
-      />
-
-      <div>
-        <label htmlFor="gender" style={{ color: "green" }}>
-          Gender:
-        </label>
-        <select id="gender" name="gender" defaultValue={male}>
-          <option value={male}>Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
+      <h2>Danh sách sản phẩm</h2>
+      {productList}
     </>
   );
 }
