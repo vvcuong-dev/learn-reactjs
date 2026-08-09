aa56af776e32f180f1960e41073377b9186f5fb3
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-09 21:33:20 +0700

    Render HTML và cấu trúc file trong ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import Header from "./header.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
+
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -32,92 +35,13 @@ function App() {
       </section>
 
       <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   );
 }
 
 export default App;
+
+/**
+ * - file: App.jsx sẽ liên kết với file index.html thông qua file main.jsx.
+ *   File main.jsx sẽ là nơi khởi tạo ứng dụng React và render component App vào phần tử có id "root" trong index.html.
+ */
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,9 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>,
 );
+
+/**
+ *  mặc dù file ./index.css không được import trực tiếp trong file App.jsx, nhưng nó vẫn có thể ảnh hưởng đến các component trong App.jsx
+ *  vì CSS được áp dụng toàn cục. Khi bạn import file CSS trong main.jsx, các style trong đó sẽ được áp dụng cho toàn bộ ứng dụng React,
+ *  bao gồm cả các component được render trong App.jsx.
+ */
