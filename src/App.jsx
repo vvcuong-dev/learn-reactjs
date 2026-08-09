9d29af5bdbf7204c8f20c360cff5d20d48b0d3e3
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-09 21:52:48 +0700

    Cú pháp JSX trong ReactJS - Phần 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
-import React from "react";
 import "./App.css";
 import Header from "./header.jsx";
 
 function App() {
-  const element = React.createElement(
-    "div",
-    { className: "title" },
-    React.createElement(
-      "h2",
-      {
-        className: "highlight",
-        id: "highlight",
-        style: { color: "blue", marginBottom: "10px" },
-      },
-      "Hello, React!",
-    ),
-
-    React.createElement(
-      "a",
-      {
-        href: "https://reactjs.org",
-        target: "_blank",
-        style: { color: "green" },
-      },
-      "Xem thêm về ReactJS",
-    ),
-
-    React.createElement(
-      "p",
-      { className: "desc", id: "desc" },
-      "Learning React is fun and exciting!",
-    ),
-  );
-
   return (
     <>
       <Header />
-      {element}
+      <div className="block">
+        <h2 className="sub-title" id="sub-title">
+          This is a block element with a background color and padding. It is
+          styled using the App.css file.
+        </h2>
+        <h1 className="title" id="title">
+          React + Vite
+        </h1>
+        <p className="description" id="description">
+          This is a simple React application created using Vite.{" "}
+          <a
+            style={{ color: "green" }}
+            href="https://vitejs.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more about Vite
+          </a>
+          .
+        </p>
+      </div>
     </>
   );
 }
