1669d2fc4edb2098d552f6708e5a92d202fd00c4
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-16 20:58:51 +0700

    Classes Component trong React JS

--- src/components/comment.component.jsx
+++ src/components/comment.component.jsx
@@ -1,10 +1,26 @@
-function Comment(props) {
-  return (
-    <div className="comment">
-      <h3 style={{ color: "blue" }}>{props.title}</h3>
-      <p style={{ color: "green" }}>{props.content}</p>
-    </div>
-  );
+import React from "react";
+
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.name = "Cuong vu";
+  }
+
+  showContent = () => {
+    console.log(this.name);
+  };
+
+  render() {
+    const { title, content } = this.props;
+    return (
+      <div className="comment">
+        <h3 style={{ color: "blue" }}>{title}</h3>
+        <p style={{ color: "green" }}>{content}</p>
+        <button onClick={this.showContent}>Show Name</button>
+      </div>
+    );
+  }
 }
 
 export default Comment;
