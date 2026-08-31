ef6a000dc92e21a49cbe2fa0fcc230129c0bc9a8
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-31 13:52:17 +0700

    Hook useState trong React JS - Phần 5 (Todo App 03)

--- src/components/todo/todo.component.jsx
+++ src/components/todo/todo.component.jsx
@@ -4,6 +4,8 @@ import { uid } from "../../ultis/uid.util";
 import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 function TodoComponent() {
   const [todoList, setTodoList] = useState([]);
@@ -23,13 +25,82 @@ function TodoComponent() {
 
     setTodoList([...todoList, todo]); // nghĩa là tạo ra một mảng mới, sao chép các phần tử từ todoList cũ và thêm phần tử todo mới vào cuối mảng
     setName("");
-    toast.success("Thêm công việc thành công!", {
+    toast.success("Add todo successfully!", {
       position: "top-right",
       autoClose: 3000,
       hideProgressBar: false, // hiển thị thanh tiến trình
       closeOnClick: true, // cho phép đóng thông báo khi người dùng nhấp vào nó
       draggable: true, // cho phép người dùng kéo thông báo để di chuyển nó
       progress: undefined, // giá trị tiến trình hiện tại của thông báo (nếu có)
+    });
+  };
+
+  const handleCompleted = (id) => {
+    const updatedTodoList = todoList.map((todo) => {
+      if (todo.id === id) {
+        if (todo.completed) {
+          toast.info(`Todo "${todo.name}" marked as incomplete`, {
+            position: "top-right",
+            autoClose: 3000,
+          });
+        } else {
+          toast.success(`Todo "${todo.name}" marked as completed`, {
+            position: "top-right",
+            autoClose: 3000,
+          });
+        }
+
+        return { ...todo, completed: !todo.completed };
+      }
+
+      return todo;
+    });
+    setTodoList(updatedTodoList);
+  };
+
+  const handleRemove = (id) => {
+    confirmAlert({
+      title: "Xác nhận xóa",
+      message: "Bạn có muốn xóa công việc này không?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => {
+            const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+            setTodoList(updatedTodoList);
+
+            toast.success("Todo removed successfully!", {
+              position: "top-right",
+              autoClose: 3000,
+            });
+          },
+        },
+        {
+          label: "No",
+        },
+      ],
+    });
+  };
+
+  const handleRemoveAll = () => {
+    confirmAlert({
+      title: "Xác nhận xóa",
+      message: "Bạn có muốn xóa tất cả công việc không?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => {
+            setTodoList([]);
+            toast.success("All todos removed successfully!", {
+              position: "top-right",
+              autoClose: 3000,
+            });
+          },
+        },
+        {
+          label: "No",
+        },
+      ],
     });
   };
 
@@ -64,10 +135,19 @@ function TodoComponent() {
                 key={id}
                 className={`todo-item d-flex justify-content-between align-items-center mt-3 ${completed ? "completed" : ""}`}
               >
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  checked={completed}
+                  onChange={() => handleCompleted(id)}
+                />
                 <span>{name}</span>
                 <div>
-                  <button className="btn btn-danger btn-sm">Xóa</button>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => handleRemove(id)}
+                  >
+                    Xóa
+                  </button>
                 </div>
               </div>
             ))}
@@ -77,7 +157,11 @@ function TodoComponent() {
               Không có công việc nào để hiển thị.
             </p>
           )}
-          <button className="btn btn-danger mt-5 d-block mx-auto" type="button">
+          <button
+            className="btn btn-danger mt-5 d-block mx-auto"
+            type="button"
+            onClick={handleRemoveAll}
+          >
             Xóa tất cả
           </button>
         </div>
