67516512164184a68a7080db4e317d70a8f17e78
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-08-31 10:00:09 +0700

    Hook useState trong React JS - Phần 2 (Xử lý Form)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import Counter from "./components/counter.component";
+import Login from "./components/login.component";
+// import Counter from "./components/counter.component";
 // import UserComponent from "./components/user.component";
 
 function App() {
   return (
     <>
       <h1>App Component - My App</h1>
-      <Counter />
+      <Login />
     </>
   );
 }
