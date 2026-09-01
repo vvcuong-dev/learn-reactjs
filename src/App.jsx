ac67cb5f62ce8e3932f13b5cd1f1bde58f23e750
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-09-01 15:34:39 +0700

    Xây dựng chức năng Reload với hook useEffect dependencies và cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import PostComponent from "./components/post-use-effect";
 
 function App() {
-  //State lưu kết quả sau khi API trả về
-
-  const [posts, setPosts] = useState([]);
-  const [isloading, setLoading] = useState(true);
-  useEffect(() => {
-    const getPosts = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts",
-      );
-      const data = await response.json();
-      setPosts(data);
-      setLoading(false);
-    };
-    getPosts();
-  }, []);
+  const [reloadKey, setReloading] = useState(false);
+  const handleReload = () => {
+    setReloading(new Date().getTime());
+  };
 
   return (
     <>
-      <h1>Call API useState + useEffect</h1>
-      {isloading ? (
-        <p>Loading...</p>
-      ) : (
-        posts.map((post) => (
-          <div key={post.id}>
-            <h2>
-              {post.id} - {post.title}
-            </h2>
-          </div>
-        ))
-      )}
+      <button onClick={handleReload}>Reload</button>
+      <PostComponent reloadKey={reloadKey} />
     </>
   );
 }
