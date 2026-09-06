d3ccd33453242147d470a14002cea37de69fb014
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-09-06 10:45:06 +0700

    update code

--- react-02/src/main.jsx
+++ react-02/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import { store } from "./stores/store.js";
 import Provider01 from "./utils/Provider01.jsx";
 
+// const store = [rootReducer, initialState]; truyền cho Provider01
 createRoot(document.getElementById("root")).render(
   <Provider01 store={store}>
     <App />
--- react-02/src/utils/Provider01.jsx
+++ react-02/src/utils/Provider01.jsx
@@ -19,3 +19,20 @@ export default function Provider01({ children, store }) {
     </Provider01Context.Provider>
   );
 }
+
+/**
+ * Provider01 nhận props từ main.jsx truyền vào, props store = [rootReducer, initialState]
+ *
+ * sau đó cập nhật state và dispatch thông qua useReducer, và truyền state và dispatch vào contextValue
+ *
+ * const [state, dispatch] = useReducer(...store);
+ *
+ * nghĩa là [state, dispatch] = useReducer(rootReducer, initialState);
+ *
+ * sau đó truyền contextValue = { state, dispatch } vào Provider01Context.Provider
+ *
+ * contextValue = { state, dispatch } sẽ được truyền vào App.jsx thông qua useContext(Provider01Context),   kết quả của contextValue sẽ là { state: { count: 0 }, dispatch: ƒ }
+ *
+ *
+ * và cuối cùng render children (App.jsx) bên trong Provider01Context.Provider
+ */
