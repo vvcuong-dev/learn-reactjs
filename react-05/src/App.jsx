13e89b08b076b1df44611cc5d59b97f3e2be0087
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-09-14 14:18:12 +0700

    Tích hợp file CSS vào ứng dụng React JS

---
+++ react-05/src/App.jsx
@@ -0,0 +1,10 @@
+import "./assets/style.css";
+import Counter from "./components/Counter/Counter";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <Counter />
+    </div>
+  );
+}
---
+++ react-05/src/components/Counter/Counter.jsx
@@ -0,0 +1,11 @@
+import "./counter.css";
+
+export default function Counter() {
+  return (
+    <div className="counter-app">
+      <h1>Counter</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  );
+}
