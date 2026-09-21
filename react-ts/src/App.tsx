46fc2223078e3fe29f93f3351c778c9af8d4b6a0
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-09-21 08:04:05 +0700

    ReactJS Hook TypeScript - Xử lý TypeScript trong Hook useState

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,60 @@
-type Props = {
-  message: string;
+import { useState } from "react";
+
+type User = {
+  name: string;
+  email: string;
 };
 
-const App = ({ message = "Hello, World!" }: Props) => {
-  return <div>{message}</div>;
-};
+function App() {
+  const [form, setForm] = useState<User | undefined | null>();
+
+  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value } as User);
+  };
+
+  const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form submitted:", form);
+  };
+
+  return (
+    <div>
+      <form
+        className="flex flex-col gap-4 p-4 border rounded-md shadow-md"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="name..."
+            className="border rounded-md p-2"
+            onChange={handleChangeValue}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="email..."
+            className="border rounded-md p-2"
+            onChange={handleChangeValue}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default App;
